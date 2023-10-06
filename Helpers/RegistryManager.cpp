53f155be627e0834149bf2db70dae6985613a381
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-10-06 15:14:31 +0300

    Add HKey enum for RegistryManager

--- Helpers/RegistryManager.cpp
+++ Helpers/RegistryManager.cpp
@@ -4,14 +4,30 @@
 #include "Logger.h"
 #include "Helpers.h"
 
+namespace {
+	HKEY ConvertToHKEY(HKey hKey) {
+		switch (hKey) {
+		case HKey::ClassesRoot:
+			return HKEY_CLASSES_ROOT;
+		case HKey::CurrentUser:
+			return HKEY_CURRENT_USER;
+		case HKey::LocalMachine:
+			return HKEY_LOCAL_MACHINE;
+		case HKey::Users:
+			return HKEY_USERS;
+		}
+		return nullptr;
+	}
+}
+
 namespace H {
-	bool RegistryManager::HasRegValue(const std::string& path, const std::string& keyName) {
-		LOG_FUNCTION_ENTER("HasRegValue({}, {})", path, keyName);
+	bool RegistryManager::HasRegValue(HKey hKey, const std::string& path, const std::string& keyName) {
+		LOG_FUNCTION_ENTER("HasRegValue({}, {}) ...", path, keyName);
 
 		std::vector<char> buff(255);
 		DWORD sz = buff.size();
 
-		auto status = RegGetValueA(HKEY_CURRENT_USER, path.c_str(), keyName.c_str(), RRF_RT_ANY | RRF_SUBKEY_WOW6464KEY, NULL, buff.data(), &sz);
+		auto status = RegGetValueA(ConvertToHKEY(hKey), path.c_str(), keyName.c_str(), RRF_RT_ANY | RRF_SUBKEY_WOW6464KEY, NULL, buff.data(), &sz);
 		if (status != S_OK) {
 			LOG_DEBUG_D("Not found reg value");
 			return false;
@@ -20,13 +36,13 @@ namespace H {
 		return true;
 	}
 
-	std::string RegistryManager::GetRegValue(const std::string& path, const std::string& keyName) {
-		LOG_FUNCTION_ENTER("GetRegValue({}, {})", path, keyName);
+	std::string RegistryManager::GetRegValue(HKey hKey, const std::string& path, const std::string& keyName) {
+		LOG_FUNCTION_ENTER("GetRegValue({}, {}) ...", path, keyName);
 
 		std::vector<char> buff(255);
 		DWORD sz = buff.size();
 
-		auto status = RegGetValueA(HKEY_CURRENT_USER, path.c_str(), keyName.c_str(), RRF_RT_ANY | RRF_SUBKEY_WOW6464KEY, NULL, buff.data(), &sz);
+		auto status = RegGetValueA(ConvertToHKEY(hKey), path.c_str(), keyName.c_str(), RRF_RT_ANY | RRF_SUBKEY_WOW6464KEY, NULL, buff.data(), &sz);
 		if (status != S_OK) {
 			LOG_WARNING_D("Can't get reg value. Status = {}", status);
 			return "";
@@ -36,10 +52,10 @@ namespace H {
 	}
 
 	// TODO: rewrite with wstring (be careful with double length of character)
-	void RegistryManager::SetRegValue(const std::string& path, const std::string& keyName, const std::string& value) {
-		LOG_FUNCTION_ENTER("SetRegValue({}, {})", path, keyName);
+	void RegistryManager::SetRegValue(HKey hKey, const std::string& path, const std::string& keyName, const std::string& value) {
+		LOG_FUNCTION_ENTER("SetRegValue({}, {}) ...", path, keyName);
 
-		auto status = RegSetKeyValueA(HKEY_CURRENT_USER, path.c_str(), keyName.c_str(), REG_SZ, value.c_str(), value.size());
+		auto status = RegSetKeyValueA(ConvertToHKEY(hKey), path.c_str(), keyName.c_str(), REG_SZ, value.c_str(), value.size());
 		if (status != S_OK) {
 			LOG_WARNING_D("Can't set reg value. Status = {}", status);
 		}
--- Helpers/RegistryManager.h
+++ Helpers/RegistryManager.h
@@ -1,12 +1,19 @@
 #pragma once
 #include <string>
 
+enum class HKey {
+    ClassesRoot,
+    CurrentUser,
+    LocalMachine,
+    Users,
+};
+
 namespace H {
     // [HKEY_CURRENT_USER; REG_SZ]
     class RegistryManager {
     public:
-        static bool HasRegValue(const std::string& path, const std::string& keyName);
-        static std::string GetRegValue(const std::string& path, const std::string& keyName);
-        static void SetRegValue(const std::string& path, const std::string& keyName, const std::string& value);
+        static bool HasRegValue(HKey hKey, const std::string& path, const std::string& keyName);
+        static std::string GetRegValue(HKey hKey, const std::string& path, const std::string& keyName);
+        static void SetRegValue(HKey hKey, const std::string& path, const std::string& keyName, const std::string& value);
     };
 }
