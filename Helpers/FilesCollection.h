23557e3cde9b7b180bdf0ae9e8932ceb73ab6765
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-11-09 17:39:33 +0200

    [Helpers] Add FilesObserver & FilesCollection classes

---
+++ Helpers/FilesCollection.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "FilesObserver.h"
+
+namespace H {
+    namespace FS {
+        class FilesCollection : public IFilesCollection {
+        public:
+            FilesCollection() = default;
+
+            void ReplaceRootPath(std::wstring newRootPath);
+
+            const std::vector<std::filesystem::path>& GetDirs() const;
+            const std::vector<std::filesystem::path>& GetFiles() const;
+            const uint64_t GetSize() const;
+
+        private:
+            void Initialize() override;
+            void HandlePathItem(const PathItem& pathItem) override;
+            void Complete() override;
+
+        private:
+            uint64_t totalSize = 0;
+            std::vector<std::filesystem::path> dirs;
+            std::vector<std::filesystem::path> files;
+        };
+    }
+}
