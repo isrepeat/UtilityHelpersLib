27c2f117ecd1d10cd0de1e0091be55ba8f109e1a
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-11-23 17:23:25 +0200

    Use "underlying_type" for Flags instead "int"

--- Helpers/Flags.h
+++ Helpers/Flags.h
@@ -1,30 +1,32 @@
 #pragma once
+#include <type_traits>
 
 namespace H {
     // TODO: add other binary operators with flags
 	template <typename Enum>
 	class Flags {
 	public:
-		static_assert(sizeof(Enum) <= sizeof(int), "Enum type must be int or less to avoid overflow Flags");
+		using UnderlyingType = std::underlying_type_t<Enum>; // for most cases it is 'int'
 
 		Flags() = default;
 		Flags(int i) : i(i) {}
-		Flags(const Enum& enm) : i((int)enm) {}
+		Flags(const Enum& enm) : i((UnderlyingType)enm) {}
 
 		Flags operator |= (Enum enm) const {
-			i |= (int)enm; return *this;
+			i |= (UnderlyingType)enm; 
+			return *this;
 		}
 		Flags operator | (Enum enm) const {
-			return Flags(i | (int)enm);
+			return Flags(i | (UnderlyingType)enm);
 		}
 		Flags operator & (Enum enm) const {
-			return Flags(i & (int)enm);
+			return Flags(i & (UnderlyingType)enm);
 		}
-		Flags operator & (int mask) const {
+		Flags operator & (UnderlyingType mask) const {
 			return Flags(i & mask);
 		}
 
-		operator int() const {
+		operator UnderlyingType() const {
 			return i;
 		}
 		//operator bool() const { // for compare with boolean values (anyway need explicitly cast with boolean compare, so you need override boolean operators ==, != ...)
@@ -32,6 +34,6 @@ namespace H {
 		//}
 
 	private:
-		int i = 0;
+		UnderlyingType i = 0;
 	};
 }
