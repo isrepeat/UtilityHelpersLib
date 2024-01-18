06723df45f3724a7ed77c14bcc018911fda6d760
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2024-01-18 14:26:17 +0200

    [H::String] Add typename to INNER_TYPE_STR

--- Helpers/Helpers.Shared/Helpers/String.hpp
+++ Helpers/Helpers.Shared/Helpers/String.hpp
@@ -60,7 +60,7 @@ namespace HELPERS_NS {
 
 #define JOIN_STRING(A, B) A B // keep space between A and B
 
-#define INNER_TYPE_STR(str) decltype(HELPERS_NS::StringDeductor(str))::type
+#define INNER_TYPE_STR(str) typename decltype(HELPERS_NS::StringDeductor(str))::type
 
 #define MAKE_STRING_T(T, str) HELPERS_NS::GetStringByType<T>(str, L""##str)
 #define MAKE_STRING_VIEW_T(T, str) HELPERS_NS::GetStringViewByType<T>(str, L""##str)
