dc64bc6ee3fc28e65a947231279d632c50c0865b
Kyrylo Stakhov <kyrylo.stakhov@dct.ua>
2024-03-25 11:21:09 +0200

    Add Semaphore helper class

---
+++ Helpers/Helpers.Shared/Helpers/Semaphore.cpp
@@ -0,0 +1,43 @@
+#include "Semaphore.h"
+#include "System.h"
+
+namespace HELPERS_NS {
+	Semaphore::Semaphore(const std::wstring& name) {
+		auto fullName = L"Global\\" + name;
+		semaphore = CreateSemaphore(nullptr, 1, 1, fullName.data());
+		auto lastError = GetLastError();
+		if (semaphore == INVALID_HANDLE_VALUE) {
+			HELPERS_NS::System::ThrowIfFailed(E_FAIL);
+		}
+	}
+
+	Semaphore::Semaphore(Semaphore&& other)
+		: semaphore(other.semaphore)
+	{
+		other.semaphore = nullptr;
+	}
+
+	Semaphore::~Semaphore() {
+		Unlock();
+		CloseHandle(semaphore);
+	}
+
+	void Semaphore::Lock() {
+		auto result = WaitForSingleObject(semaphore, INFINITE);
+		if (result != WAIT_OBJECT_0) {
+			HELPERS_NS::System::ThrowIfFailed(E_FAIL);
+		}
+	}
+
+	void Semaphore::Unlock() {
+		BOOL result = ReleaseSemaphore(semaphore, 1, nullptr);
+		if (!result) {
+			HELPERS_NS::System::ThrowIfFailed(E_FAIL);
+		}
+	}
+
+	HELPERS_NS::Scope<std::function<void()>> Semaphore::LockScoped() {
+		Lock();
+		return { [this] { Unlock(); } };
+	}
+}
