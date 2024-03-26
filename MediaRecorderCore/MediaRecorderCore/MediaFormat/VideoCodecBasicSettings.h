176878da2d5a4e795e639e2636eef963d3764bf5
Kyrylo Stakhov <kyrylo.stakhov@dct.ua>
2024-03-26 17:41:48 +0200

    Add video rate control enum to VideoCodecBasicSettings

--- MediaRecorderCore/MediaRecorderCore/MediaFormat/VideoCodecBasicSettings.h
+++ MediaRecorderCore/MediaRecorderCore/MediaFormat/VideoCodecBasicSettings.h
@@ -1,10 +1,18 @@
 #pragma once
+#include "VideoRateControlModeDef.h"
 
 #include <cstdint>
+
+enum class VideoRateControlMode {
+    VideoRateControlMode_Enum
+};
 
 struct VideoCodecBasicSettings {
     uint32_t width = 0;
     uint32_t height = 0;
+
     uint32_t bitrate = 0;
+    VideoRateControlMode rateControlMode = VideoRateControlMode::Default;
+
     uint32_t fps = 0;
 };
