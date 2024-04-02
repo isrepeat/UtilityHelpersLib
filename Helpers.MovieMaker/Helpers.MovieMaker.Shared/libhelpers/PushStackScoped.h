a651f57ef932ad7483fc3781ec3d527ffa0346ed
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2024-04-03 00:16:32 +0300

    Replace ScopedPushBase with PushStackScoped

---
+++ Helpers.MovieMaker/Helpers.MovieMaker.Shared/libhelpers/PushStackScoped.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Macros.h"
+
+template<typename StackAccessorT>
+class PushStackScoped {
+public:
+	template <typename... Args>
+	PushStackScoped(StackAccessorT* stackAccessor, Args&&... args)
+		: stackAccessor{ stackAccessor }
+	{
+		stackAccessor->Push(std::forward<Args&&>(args)...);
+	}
+
+	NO_COPY(PushStackScoped);
+
+	PushStackScoped(PushStackScoped&& other)
+		: stackAccessor(std::move(other.stackAccessor))
+	{
+		other.stackAccessor = nullptr;
+	}
+
+	PushStackScoped& operator=(PushStackScoped&& other) {
+		if (this != &other) {
+			this->stackAccessor = std::move(other.stackAccessor);
+			other.stackAccessor = nullptr;
+		}
+		return *this;
+	}
+
+	~PushStackScoped() {
+		stackAccessor->Pop();
+	}
+
+private:
+	StackAccessorT* stackAccessor;
+};
