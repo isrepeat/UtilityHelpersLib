2566c7be4b17576cded3e9207aaef2f1fe1692d7
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2024-05-13 22:42:14 +0300

    Refactor System.h

---
+++ Helpers/Helpers.Shared/Helpers/Exceptions.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "common.h"
+#include "HWindows.h"
+#include "Macros.h"
+
+#include <stdexcept>
+#include <memory>
+
+namespace CrashHandling {
+    class Backtrace;
+}
+
+namespace HELPERS_NS {
+    namespace System {
+        // NOTE: For unqie_ptr over pipmpl you need define Dtor (even default) in .cpp file
+#if COMPILE_FOR_DESKTOP
+        class ComException : public std::exception {
+        public:
+            ComException(HRESULT hr, const std::wstring& message);
+            ~ComException() = default;
+
+            ComException(const ComException&) = default;
+            ComException& operator=(const ComException&) = default;
+
+            NO_MOVE(ComException); // std::exception not support move Ctor
+
+            // [may throw NugetNotFoundException]
+            const std::shared_ptr<CrashHandling::Backtrace>& GetBacktrace() const;
+            void LogBacktrace() const;
+
+            std::wstring ErrorMessage() const;
+            HRESULT ErrorCode() const;
+
+        private:
+            std::shared_ptr<CrashHandling::Backtrace> backtrace;
+            std::wstring errorMessage;
+            HRESULT errorCode = S_OK;
+        };
+#endif
+    }
+
+
+    class NugetNotFoundException : public std::logic_error {
+    public:
+        NugetNotFoundException()
+            : std::logic_error("No nuget for this operation")
+        {}
+    };
+}
