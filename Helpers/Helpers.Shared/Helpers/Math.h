7417eb46b2ea7e147fc7d2e9553cc37f09442bf9
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2024-05-21 19:04:07 +0300

    Improve H::Size, specialize log formatter for std / our time.

--- Helpers/Helpers.Shared/Helpers/Math.h
+++ Helpers/Helpers.Shared/Helpers/Math.h
@@ -6,10 +6,32 @@ namespace HELPERS_NS {
 		unsigned int width;
 		unsigned int height;
 
-		Size(unsigned int w, unsigned int h) : width(w), height(h) {}
+		Size() = default;
 
-		bool operator != (const Size& other) {
-			return width != other.width || height != other.height;
+		Size(unsigned int width, unsigned int height)
+			: width{ width }
+			, height{ height }
+		{}
+
+		bool operator == (const Size& other) const {
+			return width == other.width && height == other.height;
+		}
+		bool operator != (const Size& other) const {
+			return !(*this == other);
 		}
 	};
+
+	inline Size operator*(Size size, float value) {
+		return Size{ 
+			static_cast<unsigned int>(size.width * value),
+			static_cast<unsigned int>(size.height * value),
+		};
+	}
+
+	inline Size operator/(Size size, float value) {
+		return Size{
+			static_cast<unsigned int>(size.width / value),
+			static_cast<unsigned int>(size.height / value),
+		};
+	}
 }
