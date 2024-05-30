d0aab38de3bb382ef49a59a9187810ad526342f0
Александр Чайковский <alexander.chaikovsky@dct.ua>
2024-05-30 15:06:41 +0300

    review fixes

--- Helpers/Helpers.Shared/Helpers/Exceptions.cpp
+++ Helpers/Helpers.Shared/Helpers/Exceptions.cpp
@@ -18,9 +18,7 @@ namespace HELPERS_NS {
             , errorMessage{message}
             , errorCode{hr}
         {
-#ifdef CRASH_HANDLING_NUGET
             LOG_ERROR_D(L"Com exception = [{:#08x}] {}", static_cast<unsigned int>(hr), message);
-#endif
         }
 
         const std::shared_ptr<CrashHandling::Backtrace>& ComException::GetBacktrace() const throw(NugetNotFoundException) {
