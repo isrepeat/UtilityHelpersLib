603c70bb3078c251e0076567be734c000b1359bd
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2024-07-24 16:18:29 +0300

    Add WinRt helpers: PerformSync, RunOnUIThread (moved from MovieMaker.Helpers)

--- Helpers/Helpers.Shared/Helpers/Action.h
+++ Helpers/Helpers.Shared/Helpers/Action.h
@@ -1,6 +1,8 @@
 #pragma once
-#include "common.h"
+#include <Helpers/common.h>
+#include <Helpers/System.h>
 #include <functional>
+#include <future>
 
 namespace HELPERS_NS {
 	// PerformActionWithAttempts also handling "std" | "any" exceptions
@@ -14,4 +16,55 @@ namespace HELPERS_NS {
 
 // use with HELPERS_NS::
 #define BreakAction  PerformActionWithAttempts::Break()
+
+
+#if COMPILE_FOR_WINRT
+    namespace WinRt {
+        inline HRESULT PerformSync(Windows::Foundation::IAsyncAction^ op) {
+            std::promise<HRESULT> prom;
+            auto fut = prom.get_future();
+
+            op->Completed = ref new Windows::Foundation::AsyncActionCompletedHandler(
+                [&](Windows::Foundation::IAsyncAction^ op, Windows::Foundation::AsyncStatus status) {
+                    HRESULT res;
+
+                    if (status == Windows::Foundation::AsyncStatus::Completed) {
+                        res = S_OK;
+                    }
+                    else {
+                        res = static_cast<HRESULT>(op->ErrorCode.Value);
+                    }
+
+                    prom.set_value(res);
+                });
+
+            auto result = fut.get();
+            return result;
+        }
+
+        void PerformSyncThrow(Windows::Foundation::IAsyncAction^ op) {
+            HELPERS_NS::System::ThrowIfFailed(PerformSync(op));
+        }
+
+        template <typename Fn>
+        inline void RunOnUIThread(Windows::UI::Core::CoreDispatcher^ uiDispatcher, Fn fn) {
+            auto currentWindow = Windows::UI::Core::CoreWindow::GetForCurrentThread();
+            if (currentWindow && currentWindow->Dispatcher == uiDispatcher) {
+                // already on UI thread
+                fn();
+            }
+            else {
+                auto task = uiDispatcher->RunAsync(
+                    Windows::UI::Core::CoreDispatcherPriority::High,
+                    ref new Windows::UI::Core::DispatchedHandler(
+                        [fn] {
+                            fn();
+                        },
+                        Platform::CallbackContext::Any)
+                );
+                PerformSyncThrow(task);
+            }
+        }
+    }
+#endif
 }
