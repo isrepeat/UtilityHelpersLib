232b9f9b8793eea96f16b8d0daca733b332abca8
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2024-07-26 13:34:51 +0300

    Refactor swapChainPanel, use dxRenderObj proxy texture for d2dTragetBitmap.

--- Helpers/Helpers.Shared/Helpers/Dx/SwapChainPanel.h
+++ Helpers/Helpers.Shared/Helpers/Dx/SwapChainPanel.h
@@ -3,79 +3,13 @@
 #include <Helpers/Dx/DxDevice.h>
 #include <Helpers/Callback.hpp>
 #include <Helpers/Flags.h>
-#include <Unknwn.h>
+
+#include "DxRenderObjProxy.h"
+#include "ISwapChainPanel.h"
+#include "FullScreenQuad.h"
 
 namespace HELPERS_NS {
 	namespace Dx {
-		// Provides an interface for an application that owns DeviceResources to be notified of the device being lost or created.
-		interface IDeviceNotify {
-			virtual void OnDeviceLost() = 0;
-			virtual void OnDeviceRestored() = 0;
-		};
-
-		enum DisplayOrientations : unsigned int {
-			None = 0,
-			Landscape = 0x1,
-			Portrait = 0x2,
-			LandscapeFlipped = 0x4,
-			PortraitFlipped = 0x8,
-		};
-
-
-		// [Guid("B34F96A3-8A71-42D3-BE89-C09777D1BB99")]
-		MIDL_INTERFACE("B34F96A3-8A71-42D3-BE89-C09777D1BB99")
-			ISwapChainPanel : public IUnknown{
-			public:
-				virtual HELPERS_NS::Dx::DxDeviceSafeObj * STDMETHODCALLTYPE GetDxDevice() = 0;
-
-				virtual void STDMETHODCALLTYPE InitSwapChainPanelInfo(
-					HELPERS_NS::Size_f logicalSize,
-					DisplayOrientations nativeOrientation,
-					DisplayOrientations currentOrientation,
-					float compositionScaleX,
-					float compositionScaleY,
-					float dpi
-				) = 0;
-
-
-				virtual void STDMETHODCALLTYPE SetLogicalSize(HELPERS_NS::Size_f logicalSize) = 0;
-				virtual void STDMETHODCALLTYPE SetNativeOrientation(DisplayOrientations nativeOrientation) = 0;
-				virtual void STDMETHODCALLTYPE SetCurrentOrientation(DisplayOrientations currentOrientation) = 0;
-				virtual void STDMETHODCALLTYPE SetDpi(float dpi) = 0;
-				virtual void STDMETHODCALLTYPE SetCompositionScale(float compositionScaleX, float compositionScaleY) = 0;
-				virtual void STDMETHODCALLTYPE SetRenderResolutionScale(float resolutionScale) = 0;
-				virtual void STDMETHODCALLTYPE ValidateDevice() = 0;
-				virtual void STDMETHODCALLTYPE HandleDeviceLost() = 0;
-				virtual void STDMETHODCALLTYPE RegisterDeviceNotify(IDeviceNotify* deviceNotify) = 0;
-				virtual void STDMETHODCALLTYPE Trim() = 0;
-				virtual void STDMETHODCALLTYPE Present() = 0;
-
-				// The size of the render target, in pixels.
-				virtual HELPERS_NS::Size_f STDMETHODCALLTYPE GetOutputSize() const = 0;
-
-				// The size of the render target, in dips.
-				virtual HELPERS_NS::Size_f STDMETHODCALLTYPE GetLogicalSize() const = 0;
-				virtual HELPERS_NS::Size_f STDMETHODCALLTYPE GetRenderTargetSize() const = 0;
-				virtual DisplayOrientations STDMETHODCALLTYPE GetNativeOrientation() const = 0;
-				virtual DisplayOrientations STDMETHODCALLTYPE GetCurrentOrientation() const = 0;
-				virtual float STDMETHODCALLTYPE GetDpi() const = 0;
-				virtual DirectX::XMFLOAT2 STDMETHODCALLTYPE GetCompositionScale() const = 0;
-
-				// D3D Accessors.
-				virtual Microsoft::WRL::ComPtr<IDXGISwapChain3> STDMETHODCALLTYPE GetSwapChain() const = 0;
-				virtual Microsoft::WRL::ComPtr<ID3D11RenderTargetView1> STDMETHODCALLTYPE GetRenderTargetView() const = 0;
-				virtual Microsoft::WRL::ComPtr<ID3D11DepthStencilView> STDMETHODCALLTYPE GetDepthStencilView() const = 0;
-				virtual D3D11_VIEWPORT STDMETHODCALLTYPE GetScreenViewport() const = 0;
-				virtual DirectX::XMFLOAT4X4	STDMETHODCALLTYPE GetOrientationTransform3D() const = 0;
-
-				// D2D Accessors.
-				virtual Microsoft::WRL::ComPtr<ID2D1Bitmap1> STDMETHODCALLTYPE GetD2DTargetBitmap() const = 0;
-				virtual D2D1::Matrix3x2F STDMETHODCALLTYPE GetOrientationTransform2D() const = 0;
-
-				virtual DXGI_COLOR_SPACE_TYPE STDMETHODCALLTYPE GetColorSpace() const = 0;
-		};
-
-
 #define _Enum_SwapChainPanelInitData_Environment \
 	Desktop, \
 	UWP,
@@ -155,7 +89,7 @@ namespace HELPERS_NS {
 
 			// D3D Accessors.
 			Microsoft::WRL::ComPtr<IDXGISwapChain3> STDMETHODCALLTYPE GetSwapChain() const override;
-			Microsoft::WRL::ComPtr<ID3D11RenderTargetView1> STDMETHODCALLTYPE GetRenderTargetView() const override;
+			Microsoft::WRL::ComPtr<ID3D11RenderTargetView> STDMETHODCALLTYPE GetRenderTargetView() const override;
 			Microsoft::WRL::ComPtr<ID3D11DepthStencilView> STDMETHODCALLTYPE GetDepthStencilView() const override;
 			D3D11_VIEWPORT STDMETHODCALLTYPE GetScreenViewport() const override;
 			DirectX::XMFLOAT4X4	STDMETHODCALLTYPE GetOrientationTransform3D() const override;
@@ -165,12 +99,14 @@ namespace HELPERS_NS {
 			D2D1::Matrix3x2F STDMETHODCALLTYPE GetOrientationTransform2D() const override;
 
 			DXGI_COLOR_SPACE_TYPE STDMETHODCALLTYPE GetColorSpace() const override;
+			bool STDMETHODCALLTYPE IsDisplayHDR10() const override;
 
 		private:
 			void CreateWindowSizeDependentResources();
 			void UpdateRenderTargetSize();
 			DXGI_MODE_ROTATION ComputeDisplayRotation();
 			void UpdateColorSpace();
+			void DrawProxyTexture();
 
 
 		private:
@@ -182,16 +118,21 @@ namespace HELPERS_NS {
 
 			// Direct3D rendering objects. Required for 3D.
 			Microsoft::WRL::ComPtr<ID3D11Texture2D> dxgiSwapChainBackBuffer;
-			Microsoft::WRL::ComPtr<ID3D11RenderTargetView1>	m_d3dRenderTargetView;
+			Microsoft::WRL::ComPtr<ID3D11RenderTargetView> m_d3dRenderTargetView;
 			Microsoft::WRL::ComPtr<ID3D11DepthStencilView> m_d3dDepthStencilView;
 
-			Microsoft::WRL::ComPtr<ID3D11Texture2D> msaaTexture;
-			Microsoft::WRL::ComPtr<ID3D11RenderTargetView1> msaaRenderTargetView;
+			//Microsoft::WRL::ComPtr<ID3D11Texture2D> msaaTexture;
+			//Microsoft::WRL::ComPtr<ID3D11RenderTargetView1> msaaRenderTargetView;
+
+			std::unique_ptr<details::DxRenderObjProxy> dxRenderObjProxy;
 
 			D3D11_VIEWPORT m_screenViewport;
 
 			// Direct2D drawing components.
 			Microsoft::WRL::ComPtr<ID2D1Bitmap1> m_d2dTargetBitmap;
+
+			// Direct3D render piplines objects.
+			std::unique_ptr<details::FullScreenQuad> fullScreenQuad;
 
 
 			// Cached device properties.
@@ -216,6 +157,7 @@ namespace HELPERS_NS {
 
 			// HDR Support
 			DXGI_COLOR_SPACE_TYPE colorSpace;
+			bool isDisplayHDR10;
 
 			// The IDeviceNotify can be held directly as it owns the DeviceResources.
 			IDeviceNotify* m_deviceNotify;
