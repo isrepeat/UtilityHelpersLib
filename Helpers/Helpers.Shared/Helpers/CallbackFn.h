c5340cbfa50413ebe23317b132b79a93248c132f
Александр Чайковский <alexander.chaikovsky@dct.ua>
2024-09-06 13:00:11 +0300

    fixes after merge

--- Helpers/Helpers.Shared/Helpers/CallbackFn.h
+++ Helpers/Helpers.Shared/Helpers/CallbackFn.h
@@ -4,32 +4,34 @@
 
 #include <functional>
 
-template<typename R, typename... Ts>
-class CallbackFn final : public ICallback<R, Ts...> {
-public:
-    CallbackFn(std::function<R(Ts...)> fn)
-        : fn(std::move(fn))
-    {}
+namespace HELPERS_NS {
+    template<typename R, typename... Ts>
+    class CallbackFn final : public ICallback<R, Ts...> {
+    public:
+        CallbackFn(std::function<R(Ts...)> fn)
+            : fn(std::move(fn))
+        {}
 
-    R Invoke(Ts... args) override {
-        return this->fn(args...);
+        R Invoke(Ts... args) override {
+            return this->fn(args...);
+        }
+
+        ICallback<R, Ts...>* Clone() const override {
+            return new CallbackFn<R, Ts...>(this->fn);
+        }
+
+    private:
+        std::function<R(Ts...)> fn;
+    };
+
+    template<typename R, typename... Ts>
+    Callback<R, Ts...> MakeCallbackFn(std::function<R(Ts...)> fn) {
+        auto icallback = std::make_unique<CallbackFn<R, Ts...>>(std::move(fn));
+        return Callback<R, Ts...>(std::move(icallback));
     }
 
-    ICallback<R, Ts...>* Clone() const override {
-        return new CallbackFn<R, Ts...>(this->fn);
+    template<typename Fn>
+    auto MakeCallbackFn(Fn fn) {
+        return MakeCallbackFn(std::function(fn));
     }
-
-private:
-    std::function<R(Ts...)> fn;
-};
-
-template<typename R, typename... Ts>
-Callback<R, Ts...> MakeCallbackFn(std::function<R(Ts...)> fn) {
-    auto icallback = std::make_unique<CallbackFn<R, Ts...>>(std::move(fn));
-    return Callback<R, Ts...>(std::move(icallback));
-}
-
-template<typename Fn>
-auto MakeCallbackFn(Fn fn) {
-    return MakeCallbackFn(std::function(fn));
 }
