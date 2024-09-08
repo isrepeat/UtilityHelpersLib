c9ddfd6f6508264a5d9a72b3d9ae5eb76cb61fd5
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2024-09-08 16:11:29 +0300

    Add new helpers macros to Preprocessor.h

--- Shared/Preprocessor/Preprocessor.h
+++ Shared/Preprocessor/Preprocessor.h
@@ -36,3 +36,209 @@
 #define NDEBUG
 #endif
 #endif
+
+
+/* ---------------------------------------- */
+/*               PP_NUM_ARG                 */
+/* ---------------------------------------- */
+#define PP_NUM_ARG_BASE( \
+      _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
+     _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
+     _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
+     _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
+     _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
+     _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
+     _61,_62,_63,N,...) N
+
+#define PP_NUM_ARG_SEQ \
+     63,62,61,60,                   \
+     59,58,57,56,55,54,53,52,51,50, \
+     49,48,47,46,45,44,43,42,41,40, \
+     39,38,37,36,35,34,33,32,31,30, \
+     29,28,27,26,25,24,23,22,21,20, \
+     19,18,17,16,15,14,13,12,11,10, \
+     9,8,7,6,5,4,3,2,1,0
+
+
+#define PP_NUM_ARG_PROXY(...) PP_EXPAND(PP_NUM_ARG_BASE(__VA_ARGS__))
+#define PP_NUM_ARG(...)  PP_NUM_ARG_PROXY(__VA_ARGS__, PP_NUM_ARG_SEQ)
+
+/* ---------------------------------------- */
+/*               PP_REVERSE                 */
+/* ---------------------------------------- */
+#define __PP_REVERSE_1(a) a
+#define __PP_REVERSE_2(a,b) b,a
+#define __PP_REVERSE_3(a,...) PP_EXPAND(__PP_REVERSE_2(__VA_ARGS__)),a
+#define __PP_REVERSE_4(a,...) PP_EXPAND(__PP_REVERSE_3(__VA_ARGS__)),a
+#define __PP_REVERSE_5(a,...) PP_EXPAND(__PP_REVERSE_4(__VA_ARGS__)),a
+#define __PP_REVERSE_6(a,...) PP_EXPAND(__PP_REVERSE_5(__VA_ARGS__)),a
+#define __PP_REVERSE_7(a,...) PP_EXPAND(__PP_REVERSE_6(__VA_ARGS__)),a
+#define __PP_REVERSE_8(a,...) PP_EXPAND(__PP_REVERSE_7(__VA_ARGS__)),a
+#define __PP_REVERSE_9(a,...) PP_EXPAND(__PP_REVERSE_8(__VA_ARGS__)),a
+#define __PP_REVERSE_10(a,...) PP_EXPAND(__PP_REVERSE_9(__VA_ARGS__)),a
+
+#define PP_REVERSE_BASE(N, ...) PP_EXPAND(__PP_REVERSE_ ## N(__VA_ARGS__))
+#define PP_REVERSE_PROXY(N, ...) PP_REVERSE_BASE(N, __VA_ARGS__)
+#define PP_REVERSE(...) PP_REVERSE_PROXY(PP_NUM_ARG(__VA_ARGS__), __VA_ARGS__)
+
+
+/* ---------------------------------------- */
+/*             PP_VARIADIC_FUNC             */
+/* ---------------------------------------- */
+#define PP_VARIADIC_FUNC(Func, ...) PP_CONCAT(Func, PP_NUM_ARG(__VA_ARGS__)) (__VA_ARGS__)
+
+
+/* ---------------------------------------- */
+/*         PP_GET_XXX / PP_DROP_XXX         */
+/* ---------------------------------------- */
+#define PP_GET_ELEMENT_N_BASE(_1, _2, _3, _4, _5, N, ...) N
+#define PP_GET_ELEMENT_N(...) PP_EXPAND(PP_GET_ELEMENT_N_BASE(__VA_ARGS__))
+
+
+#define PP_GET_FIRST_BASE(arg1, ...) arg1
+#define PP_GET_FIRST_PROXY(...) PP_EXPAND(PP_GET_FIRST_BASE(__VA_ARGS__))
+#define PP_GET_FIRST(...) PP_GET_FIRST_PROXY(__VA_ARGS__)
+
+#define PP_GET_LAST(...) PP_GET_FIRST(PP_REVERSE(__VA_ARGS__))
+
+
+#define PP_DROP_FIRST_BASE(arg1, ...) __VA_ARGS__
+#define PP_DROP_FIRST_PROXY(...) PP_EXPAND(PP_DROP_FIRST_BASE(__VA_ARGS__))
+#define PP_DROP_FIRST(...) PP_DROP_FIRST_PROXY(__VA_ARGS__)
+
+#define PP_DROP_LAST(...) PP_REVERSE(PP_DROP_FIRST(PP_REVERSE(__VA_ARGS__)))
+
+
+//#define PP_SEQ 5, 4, 3, 2, 1
+//
+//#define PP_GET_REST_FILLED_ARGS_BASE(_1, _2, _3, _4, _5, ...) __VA_ARGS__
+//#define PP_GET_REST_FILLED_ARGS_PROXY(...) PP_EXPAND(PP_GET_REST_FILLED_ARGS_BASE(__VA_ARGS__))
+//#define PP_GET_REST_FILLED_ARGS(...) PP_GET_REST_FILLED_ARGS_PROXY(__VA_ARGS__, PP_SEQ)
+//
+//#define PP_GET_MOST_FILLED_ARGS_BASE(_1, _2, _3, _4, _5, ...) _1, _2, _3, _4, _5
+//#define PP_GET_MOST_FILLED_ARGS_PROXY(...) PP_EXPAND(PP_GET_MOST_FILLED_ARGS_BASE(__VA_ARGS__))
+//#define PP_GET_MOST_FILLED_ARGS(...) PP_GET_MOST_FILLED_ARGS_PROXY(__VA_ARGS__, PP_SEQ)
+
+// [version 2]
+//
+// Example:
+// PP_GET_LAST(a,b,c): 
+//   Count = 3; (<= Max)
+// 
+// PP_GET_ELEMENT_N(PP_GET_REST_FILLED_ARGS(a, b, c), PP_REVERSE(PP_GET_MOST_FILLED_ARGS(a, b, c)))
+// PP_GET_ELEMENT_N((a, b, c, _N[Count], ... , _N[1]), PP_REVERSE((a, b, c, _N[Max], ... , _N[Count+1])))
+// PP_GET_ELEMENT_N(a, b, c, _N[Count], ... , _N[1], _N[Count+1], ... , _N[Max], c, b, a)
+//                           \________________ placeholders __________________/  ^
+//                                                                               |
+//                                                                            result ('N')
+//#define PP_GET_LAST(...) PP_GET_ELEMENT_N(PP_GET_MOST_FILLED_ARGS(__VA_ARGS__), PP_REVERSE(__VA_ARGS__));
+
+
+
+/* ---------------------------------------- */
+/*               PP_FOR_EACH                */
+/* ---------------------------------------- */
+// TODO: Rename some macros more readable.
+/* because gcc cpp doesn't recursively expand macros, so a single CALLIT
+ * macro can't be used in all the FE_n macros below
+ */
+#define FE_CALLITn01(a,b)  a b
+#define FE_CALLITn02(a,b)  a b
+#define FE_CALLITn03(a,b)  a b 
+#define FE_CALLITn04(a,b)  a b
+#define FE_CALLITn04(a,b)  a b
+#define FE_CALLITn05(a,b)  a b
+#define FE_CALLITn06(a,b)  a b
+#define FE_CALLITn07(a,b)  a b
+#define FE_CALLITn08(a,b)  a b
+#define FE_CALLITn09(a,b)  a b
+#define FE_CALLITn10(a,b)  a b
+#define FE_CALLITn11(a,b)  a b
+#define FE_CALLITn12(a,b)  a b
+#define FE_CALLITn13(a,b)  a b
+#define FE_CALLITn14(a,b)  a b
+#define FE_CALLITn15(a,b)  a b
+#define FE_CALLITn16(a,b)  a b
+#define FE_CALLITn17(a,b)  a b
+#define FE_CALLITn18(a,b)  a b
+#define FE_CALLITn19(a,b)  a b
+#define FE_CALLITn20(a,b)  a b
+#define FE_CALLITn21(a,b)  a b
+
+ /* the MSVC preprocessor expands __VA_ARGS__ as a single argument, so it needs
+  * to be expanded indirectly through the CALLIT macros.
+  * http://connect.microsoft.com/VisualStudio/feedback/details/380090/variadic-macro-replacement
+  * http://stackoverflow.com/questions/21869917/visual-studio-va-args-issue
+  */
+#define FE_n00()
+#define FE_n01(what, a, ...)  what(a)
+#define FE_n02(what, a, ...)  what(a) FE_CALLITn02(FE_n01,(what, ##__VA_ARGS__))
+#define FE_n03(what, a, ...)  what(a) FE_CALLITn03(FE_n02,(what, ##__VA_ARGS__))
+#define FE_n04(what, a, ...)  what(a) FE_CALLITn04(FE_n03,(what, ##__VA_ARGS__))
+#define FE_n05(what, a, ...)  what(a) FE_CALLITn05(FE_n04,(what, ##__VA_ARGS__))
+#define FE_n06(what, a, ...)  what(a) FE_CALLITn06(FE_n05,(what, ##__VA_ARGS__))
+#define FE_n07(what, a, ...)  what(a) FE_CALLITn07(FE_n06,(what, ##__VA_ARGS__))
+#define FE_n08(what, a, ...)  what(a) FE_CALLITn08(FE_n07,(what, ##__VA_ARGS__))
+#define FE_n09(what, a, ...)  what(a) FE_CALLITn09(FE_n08,(what, ##__VA_ARGS__))
+#define FE_n10(what, a, ...)  what(a) FE_CALLITn10(FE_n09,(what, ##__VA_ARGS__))
+#define FE_n11(what, a, ...)  what(a) FE_CALLITn11(FE_n10,(what, ##__VA_ARGS__))
+#define FE_n12(what, a, ...)  what(a) FE_CALLITn12(FE_n11,(what, ##__VA_ARGS__))
+#define FE_n13(what, a, ...)  what(a) FE_CALLITn13(FE_n12,(what, ##__VA_ARGS__))
+#define FE_n14(what, a, ...)  what(a) FE_CALLITn14(FE_n13,(what, ##__VA_ARGS__))
+#define FE_n15(what, a, ...)  what(a) FE_CALLITn15(FE_n14,(what, ##__VA_ARGS__))
+#define FE_n16(what, a, ...)  what(a) FE_CALLITn16(FE_n15,(what, ##__VA_ARGS__))
+#define FE_n17(what, a, ...)  what(a) FE_CALLITn17(FE_n16,(what, ##__VA_ARGS__))
+#define FE_n18(what, a, ...)  what(a) FE_CALLITn18(FE_n17,(what, ##__VA_ARGS__))
+#define FE_n19(what, a, ...)  what(a) FE_CALLITn19(FE_n18,(what, ##__VA_ARGS__))
+#define FE_n20(what, a, ...)  what(a) FE_CALLITn20(FE_n19,(what, ##__VA_ARGS__))
+#define FE_n21(what, a, ...)  what(a) FE_CALLITn21(FE_n20,(what, ##__VA_ARGS__))
+#define FE_n22(...)           ERROR: FOR_EACH only supports up to 21 arguments
+
+#define FE_GET_MACRO(_0,_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,_15,_16,_17,_18,_19,_20,_21,_22,NAME,...) NAME
+#define PP_FOR_EACH(what, ...) FE_CALLITn01(FE_GET_MACRO(_0, ##__VA_ARGS__,FE_n22,FE_n21,FE_n20,FE_n19, \
+                            FE_n18,FE_n17,FE_n16,FE_n15,FE_n14,FE_n13,FE_n12,FE_n11,FE_n10,FE_n09,\
+                            FE_n08,FE_n07,FE_n06,FE_n05,FE_n04,FE_n03,FE_n02,FE_n01,FE_n00), (what, ##__VA_ARGS__))
+
+
+
+/* ---------------------------------------- */
+/*   PP_INLINE_TEMPLATE_SPECIALIZATION      */
+/* ---------------------------------------- */
+#define __PP_CLOSE_NAMESPACE(value) }
+#define __PP_OPEN_NAMESPACE(value) namespace value {
+
+#define PP_INLINE_TEMPLATE_SPECIALIZATION_INTERNAL(code, ...) \
+    PP_FOR_EACH(__PP_CLOSE_NAMESPACE, __VA_ARGS__) \
+    code \
+    PP_FOR_EACH(__PP_OPEN_NAMESPACE, __VA_ARGS__) 
+
+#define PP_INLINE_TEMPLATE_SPECIALIZATION(...) \
+    PP_INLINE_TEMPLATE_SPECIALIZATION_INTERNAL(PP_GET_LAST(__VA_ARGS__), PP_DROP_LAST(__VA_ARGS__))
+
+// Usage:
+// Redefine __NAMESPACE__ macro according to current namespace before call _BEGIN().
+//
+// For example: 
+// namespace A {
+//   namespace B::C {
+//      ...
+//      #define __NAMESPACE__ A, B::C
+//      PP_INLINE_TEMPLATE_SPECIALIZATION_BEGIN()
+//      ...
+//      PP_INLINE_TEMPLATE_SPECIALIZATION_END()
+#define PP_INLINE_TEMPLATE_SPECIALIZATION_BEGIN() \
+    PP_FOR_EACH(__PP_CLOSE_NAMESPACE, __NAMESPACE__)
+
+#define PP_INLINE_TEMPLATE_SPECIALIZATION_END() \
+    PP_FOR_EACH(__PP_OPEN_NAMESPACE, __NAMESPACE__)
+
+
+//
+// Heleprs to get last namespace from __NAMESPACE__'s macro
+//
+
+#define __PP_JOIN_NAMESPACE(value) ::value
+
+// Get full namespace from __NAMESPACE__'s macro args
+#define PP_LAST_NAMESPACE \
+	PP_GET_FIRST(__NAMESPACE__) PP_FOR_EACH(__PP_JOIN_NAMESPACE, PP_DROP_FIRST(__NAMESPACE__))
