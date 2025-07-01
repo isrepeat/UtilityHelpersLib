375ac0cc3cd68942757d8ea49aaefc9d9e61213c
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2025-07-01 14:11:10 +0300

    [HelpersCs] Write TriggeredAction

--- HelpersCs/Helpers/RelayCommand.cs
+++ HelpersCs/Helpers/RelayCommand.cs
@@ -7,16 +7,16 @@ namespace Helpers {
         private readonly Func<bool> _canExecute;
 
         public RelayCommand(Action execute, Func<bool> canExecute = null) {
-            this._execute = execute ?? throw new ArgumentNullException(nameof(execute));
-            this._canExecute = canExecute;
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
         }
 
         public bool CanExecute(object parameter) {
-            return this._canExecute?.Invoke() ?? true;
+            return _canExecute?.Invoke() ?? true;
         }
 
         public void Execute(object parameter) {
-            this._execute();
+            _execute();
         }
 
         public event EventHandler CanExecuteChanged {
@@ -31,16 +31,16 @@ namespace Helpers {
         private readonly Func<T, bool> _canExecute;
 
         public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null) {
-            this._execute = execute ?? throw new ArgumentNullException(nameof(execute));
-            this._canExecute = canExecute;
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
         }
 
         public bool CanExecute(object parameter) {
-            return this._canExecute?.Invoke((T)parameter) ?? true;
+            return _canExecute?.Invoke((T)parameter) ?? true;
         }
 
         public void Execute(object parameter) {
-            this._execute((T)parameter);
+            _execute((T)parameter);
         }
 
         public event EventHandler CanExecuteChanged {
