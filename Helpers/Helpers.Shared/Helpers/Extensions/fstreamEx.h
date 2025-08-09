740531ff0fec2ef3d77974030ab986af025373d5
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2025-08-09 14:30:32 +0300

    [Helpers] Add Stream.h; Refactor fstreamEx.h, write std::ex::ofstream class.

--- Helpers/Helpers.Shared/Helpers/Extensions/fstreamEx.h
+++ Helpers/Helpers.Shared/Helpers/Extensions/fstreamEx.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Helpers/common.h"
 #include "Helpers/Logger.h"
+#include "Helpers/Stream.h"
 #include "Helpers/Regex.h"
 
 #include <optional>
@@ -10,23 +11,109 @@
 
 
 namespace STD_EXT_NS {
-	struct fstream_not_opened : ::std::exception {
-		fstream_not_opened()
-			: ::std::exception("stream not opened") {
-		}
+	enum class UTFByteOrderMark {
+		UTF8,
+		UTF16_BE,
+		UTF16_LE,
+		UTF32_BE,
+		UTF32_LE
 	};
 
-	namespace details {
-		int FindFileCodePageInFirstLineOfStream(::std::ifstream& fileStream) {
-			if (!fileStream.is_open()) {
-				throw ::std::ex::fstream_not_opened{};
+
+	struct ifstream : public ::std::ifstream {
+		using ::std::ifstream::basic_ifstream;
+
+		static const inline ::std::unordered_map<UTFByteOrderMark, ::std::vector<uint8_t>> KnownUTFByteOrderMarks = {
+			{ UTFByteOrderMark::UTF8, { 0xEF, 0xBB, 0xBF } },
+			{ UTFByteOrderMark::UTF16_BE, { 0xFE, 0xFF } },
+			{ UTFByteOrderMark::UTF16_LE, { 0xFF, 0xFE } },
+			{ UTFByteOrderMark::UTF32_BE, { 0x00, 0x00, 0xFE, 0xFF } },
+			{ UTFByteOrderMark::UTF32_LE, { 0xFF, 0xFE, 0x00, 0x00 } },
+		};
+
+		struct Data {
+			::std::vector<char> byteArray;
+			::std::optional<int> codePage;
+		};
+
+
+		Data ReadData() {
+			HELPERS_NS::Stream::ThrowIfStreamNotOpened(*this, "ReadData");
+			HELPERS_NS::Stream::ThrowIfStreamFailed(*this, "ReadData");
+
+			auto& ifstreamRef = static_cast<::std::ifstream&>(*this);
+
+			this->SkipUTFByteOrderMark();
+
+			Data data;
+			data.codePage = this->DetectCodePageInFirstLineOfStream();
+
+			data.byteArray = ::std::vector<char>{
+				::std::istreambuf_iterator<char>(ifstreamRef),
+				::std::istreambuf_iterator<char>()
+			};
+			return data;
+		}
+
+
+		::std::optional<UTFByteOrderMark> DetectUTFByteOrderMark() {
+			::std::istream::pos_type originalPos = this->tellg();
+			if (originalPos != 0) {
+				throw std::runtime_error("DetectUTFByteOrderMark must be called at the beginning of the stream");
 			}
 
-			auto prevFileStreamPointerPos = fileStream.tellg();
-			fileStream.seekg(0, ::std::ios::beg);
+			// „итаем максимум N байт (по самой длинной сигнатуре BOM)
+			size_t maxLength = 0;
+			for (const auto& [_, bom] : KnownUTFByteOrderMarks) {
+				maxLength = std::max(maxLength, bom.size());
+			}
+
+			::std::vector<uint8_t> buffer(maxLength);
+			this->read(reinterpret_cast<char*>(buffer.data()), static_cast<::std::streamsize>(maxLength));
+			HELPERS_NS::Stream::ThrowIfStreamFailed(*this, "DetectUTFByteOrderMark");
+
+			auto readBytes = this->gcount();
+
+			this->seekg(originalPos);
+
+			// ѕровер€ем наличие любого из BOM
+			for (const auto& [bomType, bom] : KnownUTFByteOrderMarks) {
+				if (static_cast<size_t>(readBytes) < bom.size()) {
+					continue; // недостаточно байт дл€ этого BOM
+				}
+
+				if (::std::equal(bom.begin(), bom.end(), buffer.begin())) {
+					return bomType;
+				}
+			}
+
+			return ::std::nullopt;
+		}
+
+	private:
+		void SkipUTFByteOrderMark() {
+			::std::istream::pos_type originalPos = this->tellg();
+			if (originalPos != 0) {
+				throw std::runtime_error("SkipUTFByteOrderMarkInStream must be called at the beginning of the stream (tellg() == 0)");
+			}
+
+			auto bomType = this->DetectUTFByteOrderMark();
+			if (!bomType) {
+				return;
+			}
+
+			const auto& bom = KnownUTFByteOrderMarks.at(*bomType);
+			this->seekg(static_cast<std::streamoff>(bom.size()), std::ios::beg); // ѕропускаем UTF BOM
+			HELPERS_NS::Stream::ThrowIfStreamFailed(*this, "SkipUTFByteOrderMark");
+		}
+
+
+		::std::optional<int> DetectCodePageInFirstLineOfStream() {
+			auto prevFileStreamPointerPos = this->tellg();
+			this->seekg(0, ::std::ios::beg);
 
 			::std::string firstLine;
-			if (::std::getline(fileStream, firstLine)) {
+			if (::std::getline(*this, firstLine)) {
 				auto matches = HELPERS_NS::Regex::GetRegexMatches(firstLine, ::std::regex("CodePage\\s*=\\s*(\\d+)"));
 				if (!matches.empty()) {
 					if (matches[0].capturedGroups.size() > 1) {
@@ -36,76 +123,20 @@ namespace STD_EXT_NS {
 				}
 			}
 
-			fileStream.seekg(prevFileStreamPointerPos, std::ios::beg);
-			return -1;
+			this->seekg(prevFileStreamPointerPos, ::std::ios::beg);
+			return ::std::nullopt;
 		}
-
-		const ::std::vector<::std::vector<::std::uint8_t>> utfByteOrderMarks = {
-			{ 0xEF, 0xBB, 0xBF }, // UTF8
-		};
-
-		bool CheckIfBytesArrayIsBOM(const ::std::vector<::std::uint8_t>& utfByteOrderMark, const ::std::vector<::std::uint8_t>& firstSomeBytes) {
-			LOG_ASSERT(utfByteOrderMark.size() <= firstSomeBytes.size());
-
-			for (int idx = 0; idx < utfByteOrderMark.size(); idx++) {
-				if (utfByteOrderMark[idx] != firstSomeBytes[idx]) {
-					return false;
-				}
-			}
-			return true;
-		}
-
-		void SkipUTFByteOrderMarkInStream(::std::ifstream& fileStream) {
-			if (!fileStream.is_open()) {
-				throw std::ex::fstream_not_opened{};
-			}
-
-			auto prevFileStreamPointerPos = fileStream.tellg();
-			if (prevFileStreamPointerPos != 0) {
-				fileStream.seekg(0, std::ios::beg);
-				LOG_WARNING_D("prevFileStreamPointerPos (= {}) was not point to begin, but after this function finish it will", static_cast<int>(prevFileStreamPointerPos));
-			}
-
-			std::vector<uint8_t> firstSomeBytes(10);
-			fileStream.read((char*)firstSomeBytes.data(), firstSomeBytes.size());
-
-			for (auto& utfByteOrderMark : utfByteOrderMarks) {
-				if (CheckIfBytesArrayIsBOM(utfByteOrderMark, firstSomeBytes)) {
-					fileStream.seekg(utfByteOrderMark.size(), std::ios::beg);
-					return;
-				}
-			}
-			fileStream.seekg(0, std::ios::beg);
-		}
-	} // namespace details
+	};
 
 
-	struct ifstream : public ::std::ifstream {
-		using ::std::ifstream::basic_ifstream;
 
-		struct Data {
-			::std::vector<char> byteArray;
-			::std::optional<int> codePage;
-		};
+	struct ofstream : public ::std::ofstream {
+		using ::std::ofstream::ofstream;
 
-		Data ReadData() {
-			if (!this->is_open()) {
-				throw std::ex::fstream_not_opened{};
-			}
-			auto& ifstreamRef = static_cast<::std::ifstream&>(*this);
-
-			details::SkipUTFByteOrderMarkInStream(ifstreamRef);
-
-			Data data;
-			auto codePage = details::FindFileCodePageInFirstLineOfStream(ifstreamRef);
-			if (codePage != -1) {
-				data.codePage = codePage;
-			}
-			data.byteArray = ::std::vector<char>{
-				::std::istreambuf_iterator<char>(ifstreamRef),
-				::std::istreambuf_iterator<char>()
-			};
-			return data;
+		void WriteUTFByteOrderMark(UTFByteOrderMark bomType = UTFByteOrderMark::UTF8) {
+			const auto& bom = ifstream::KnownUTFByteOrderMarks.at(bomType);
+			this->write(reinterpret_cast<const char*>(bom.data()), static_cast<std::streamsize>(bom.size()));
+			HELPERS_NS::Stream::ThrowIfStreamFailed(*this, "WriteUTF8ByteOrderMark");
 		}
 	};
 }
