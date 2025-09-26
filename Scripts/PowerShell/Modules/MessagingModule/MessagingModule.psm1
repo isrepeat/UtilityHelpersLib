bce43024885c855f48c3bdf972337f0b954d1637
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2025-09-26 16:17:19 +0300

    ...

--- Scripts/PowerShell/Modules/MessagingModule/MessagingModule.psm1
+++ Scripts/PowerShell/Modules/MessagingModule/MessagingModule.psm1
@@ -1,11 +1,67 @@
+function NewLine {
+    Write-Host ""
+}
+
+
+<# 
 function Message {
     param (
         [string]$color = "White",
         [string]$prefix = "",
         [string]$text
     )
+    Write-Host "$prefix$text" -ForegroundColor $color
+}
+#>
+
+# ѕример использовани€:
+# Message "Hello"
+# Message -text "Hello"
+# Message -color Red "Hello"
+function Message {
+    param(
+        [Parameter(Position = 0, Mandatory = $true)]
+        [string]$text,
+        [string]$color = "White",
+        [string]$prefix = ""
+    )
+
+    # ѕроверка: текст должен быть последним аргументом.
+	# аккуратно разбираем исходную строку вызова
+    $line = $MyInvocation.Line.TrimEnd() # убираем хвостовые пробелы/CRLF
+    $tokens = [regex]::Split($line, "\s+") | Where-Object { $_ -ne "" }
+
+    for ($i = 0; $i -lt $tokens.Length; $i++) {
+        Write-Host "argsOrder[$i] = $($tokens[$i])"
+    }
+
+    # ЅерЄм последний токен, разворачиваем переменные/подвыражени€ и снимаем внешние кавычки
+    $lastRaw = $tokens[-1]
+    $lastExpanded = $ExecutionContext.InvokeCommand.ExpandString($lastRaw)
+    $lastToken = $lastExpanded.Trim("'`"")
+
+    if ($lastToken -ne $text) {
+		Write-Host -ForegroundColor Red "[ERROR] lastToken ($lastToken) != text ($text)"
+        throw "Text argument must be the last parameter."
+    }
 
     Write-Host "$prefix$text" -ForegroundColor $color
+}
+
+function MessageError {
+    param (
+        [Parameter(Position = 0, Mandatory = $true)]
+        [string]$text
+    )
+    Message -color Red -text $text
+}
+
+function MessageAction {
+    param (
+		[Parameter(Position = 0, Mandatory = $true)]
+        [string]$text
+    )
+    Message -color Yellow -text $text
 }
 
 
@@ -23,10 +79,6 @@ function TestColoredMessagePalette {
     }
 }
 
-function NewLine {
-    Write-Host ""
-}
-
 
 function WrapInQuotesIfNeeded {
     param ([string]$text)
@@ -41,6 +93,8 @@ function WrapInQuotesIfNeeded {
 
 Export-ModuleMember -Function @(
     'Message',
+    'MessageError',
+    'MessageAction',
     'NewLine',
     'TestColoredMessagePalette',
     'WrapInQuotesIfNeeded'
